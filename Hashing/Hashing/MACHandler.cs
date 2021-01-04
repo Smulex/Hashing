911597a02f297752c615f9dfb2dd52dcc060e702
Christian Bøgh <chri45i4@zbc.dk>
2021-01-04 20:59:37 +0100

    .
    Fejl i MAC ASCII

---
+++ Hashing/Hashing/MACHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Hashing
+{
+    class MACHandler
+    {
+        private HMAC myMAC;
+        public MACHandler(string name)
+        {
+            if (name.CompareTo("SHA1") == 0)
+            {
+                myMAC = new HMACSHA1();
+            }
+            if (name.CompareTo("MD5") == 0)
+            {
+                myMAC = new HMACMD5();
+            }
+            if (name.CompareTo("RIPEMD") == 0)
+            {
+                myMAC = new HMACRIPEMD160();
+            }
+            if (name.CompareTo("SHA256") == 0)
+            {
+                myMAC = new HMACSHA256();
+            }
+            if (name.CompareTo("SHA384") == 0)
+            {
+                myMAC = new HMACSHA384();
+            }
+            if (name.CompareTo("SHA512") == 0)
+            {
+                myMAC = new HMACSHA512();
+            }
+        }
+
+        public byte[] ComputeMAC(byte[] mes, byte[] key)
+        {
+            myMAC.Key = key;
+            return myMAC.ComputeHash(mes);
+        }
+        public bool CheckAuthenticity(byte[] mes, byte[] mac, byte[] key)
+        {
+            myMAC.Key = key;
+            if (CompareByteArrays(myMAC.ComputeHash(mes), mac, myMAC.HashSize / 8) == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool CompareByteArrays(byte[] a, byte[] b, int len)
+        {
+            for (int i = 0; i < len; i++)
+                if (a[i] != b[i]) return false;
+            return true;
+        }
+    }
+}
---
+++ Hashing/Hashing/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hashing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ConversionHandler myConverter = new ConversionHandler();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComputeMACBTN_Click(object sender, RoutedEventArgs e)
+        {
+            MACHandler mh = new MACHandler(selectionBox.Text);
+            byte[] mac = mh.ComputeMAC(myConverter.StringToByteArray(plainTextTXT.Text), myConverter.StringToByteArray(keyTXT.Text));
+
+            macASCIITXT.Text = myConverter.ByteArrayToString(mac);
+            macHEXTXT.Text = myConverter.ByteArrayToHexString(mac);
+        }
+
+        private void VerifyMACBTN_Click(object sender, RoutedEventArgs e)
+        {
+            MACHandler mh = new MACHandler(selectionBox.Text);
+            if (mh.CheckAuthenticity(myConverter.StringToByteArray(plainTextTXT.Text), myConverter.HexStringToByteArray(macHEXTXT.Text), myConverter.StringToByteArray(keyTXT.Text)) == true)
+            {
+                MessageBox.Show("MAC er validt");
+            }
+            else
+            {
+                MessageBox.Show("MAC er ikke validt");
+            }
+        }
+    }
+}
